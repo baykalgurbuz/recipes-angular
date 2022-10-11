58f46c66e3be5407b82e3c958376cc3587384c61
enes baykal gurbuz <enesgurbuz@lastikcim.com.tr>
2022-10-12 00:35:46 +0300

    Use ngfor and comminucate other component

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] =[
+    new Recipe('A Test Recipe','This is simple a test','https://images.immediate.co.uk/production/volatile/sites/30/2022/06/Chicken-chow-mein-7aeec1d.png'),
+    new Recipe('A Test Recipe2','This is simple a test2','https://images.immediate.co.uk/production/volatile/sites/30/2022/06/Chicken-chow-mein-7aeec1d.png')
+
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
