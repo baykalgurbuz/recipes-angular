cb746ee835916578e2d2f7a0a9b04aa9ec18d345
enes baykal gurbuz <enesgurbuz@lastikcim.com.tr>
2022-10-14 01:10:18 +0300

    Event and Property binding recipe data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basics-angular';
+  loaddedFeature='recipe';
+  onNavigate(feature:string){
+    this.loaddedFeature=feature;
+  }
 }
