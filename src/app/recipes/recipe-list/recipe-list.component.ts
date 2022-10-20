143821c2afee29bc62a1965c81cf89ee3f7325ab
enes baykal gurbuz <enesgurbuz@lastikcim.com.tr>
2022-10-20 14:22:47 +0300

    Add ingredients to Shopping List

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+ @Output() recipeWasSelected =new EventEmitter<Recipe>();
   recipes:Recipe[] =[
     new Recipe('A Test Recipe','This is simple a test','https://images.immediate.co.uk/production/volatile/sites/30/2022/06/Chicken-chow-mein-7aeec1d.png'),
     new Recipe('A Test Recipe2','This is simple a test2','https://images.immediate.co.uk/production/volatile/sites/30/2022/06/Chicken-chow-mein-7aeec1d.png')
@@ -17,5 +18,7 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe)
+  }
 }
